c6f50b0cb9a92dc4b3c18ce6eed051a5db5d4e11
MadCom96 <70102600+MadCom96@users.noreply.github.com>
2022-04-16 20:21:14 +0900

    Update main.cpp

--- 13460_구슬_탈출_2/main.cpp
+++ 13460_구슬_탈출_2/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #define firstMoved board[first->y + diry[dir]][first->x + dirx[dir]]
 #define secondMoved board[second->y + diry[dir]][second->x + dirx[dir]]
@@ -11,7 +10,7 @@ typedef struct {
 
 string board[10] = { "", };
 // up left down right
-int dirx[] = { 0, -1, 0, 1 }; 
+int dirx[] = { 0, -1, 0, 1 };
 int diry[] = { -1, 0, 1, 0 };
 // simulation recording
 queue<at> Rat;
@@ -19,8 +18,6 @@ queue<at> Bat;
 queue<int> len;
 
 int move(at R, at B, int dir) {
-	bool stopped1 = false;
-	bool stopped2 = false;
 	at* first = &R;
 	at* second = &B;
 	if (dir == 0) {
@@ -48,16 +45,14 @@ int move(at R, at B, int dir) {
 		}
 
 	}
-		
+
 	while (true) { // first move
 		if (firstMoved == 'O') {
 			first->x += dirx[dir];
 			first->y += diry[dir];
-			stopped1 = true;
 			break;
 		}
 		if (firstMoved == '#') {
-			stopped1 = true;
 			break;
 		}
 		first->x += dirx[dir];
@@ -67,20 +62,17 @@ int move(at R, at B, int dir) {
 		if (secondMoved == 'O') {
 			second->x += dirx[dir];
 			second->y += diry[dir];
-			stopped2 = true;
 			break;
 		}
 		if (secondMoved == '#') {
-			stopped2 = true;
 			break;
 		}
 		if (second->x + dirx[dir] == first->x && second->y + diry[dir] == first->y) {
-			stopped2 = true;
 			break;
 		}
 		second->x += dirx[dir];
 		second->y += diry[dir];
-	
+
 	}
 	//both stoepped
 	if (board[B.y][B.x] == 'O') // if b's out, failed whatever
@@ -96,14 +88,14 @@ int move(at R, at B, int dir) {
 int main() {
 	int n, m; // vertical, horizontal,, 3~10
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 		cin >> board[i];
 
 	at R{ 0, 0 };
 	at B{ 0, 0 };
 
 	//find R, B 
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) {
 			if (board[i][j] == '#' || board[i][j] == '.') {
 				continue;
@@ -135,7 +127,7 @@ int main() {
 			return 0;
 		}
 		for (int i = 0; i < 4; i++) {
-			int result = move(R, B, i);
+			result = move(R, B, i);
 			if (result == 1) {
 				cout << l;
 				return 0;
