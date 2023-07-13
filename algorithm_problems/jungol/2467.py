6c1402ee35d04d84f8a3f7f70a699e2dd92bb0f5
Jinha Hwang <70102600+MadCom96@users.noreply.github.com>
2023-07-13 10:56:07 +0900

    Update 2467.py

--- algorithm_problems/jungol/2467.py
+++ algorithm_problems/jungol/2467.py
@@ -1,7 +1,7 @@
 # 역순으로 해서 연결하면서 유니온파인드 하면 되지 않을까?
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-edges.sort(reversed= True)
+edges.sort(key= lambda x: -x[2])
 # 일단 노드 번호는 1 ~ n이라고 가정하고 풀자. 조건이 없는듯
 
 lim = 10*9
@@ -13,7 +13,9 @@ def find(node) -> int:
     if node == roots[node]:
         return node
     else:
-        return find(roots[node])
+        root = find(roots[node])
+        roots[node] = root
+        return root
 
 def union(a, b):
     global roots
@@ -24,3 +26,8 @@ def union(a, b):
 for edge in edges:
     # 한 줄씩 추가하면서 유니온 파인드
     # 15 6 3
+    a, b, w = edge
+    union(a, b)
+    if roots[
+
+        # 새로 연결을 찾아야됨
